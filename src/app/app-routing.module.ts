253ee274654e311269e326328c73da6a328f07ee
tangsiner2019 <tangsiner@qq.com>
2020-03-09 18:55:35 +0800

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'main', loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule) },
+  { path: '',  redirectTo: 'main', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
